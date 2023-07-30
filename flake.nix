ed69163f4e1456a8866b51f3cd70ae85d1194f69
Alexander Bakker <ab@alexbakker.me>
2023-07-30 22:19:51 +0200

    Release v0.1.2

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
   flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = nixpkgs.legacyPackages.${system};
-      gotchetVersion = "0.1.1";
+      gotchetVersion = "0.1.2";
     in rec {
       packages = flake-utils.lib.flattenTree rec {
         default = gotchet-cli;
