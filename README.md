c652728b003e582d146071395bb0e7f93e8eafef
Alexander Bakker <ab@alexbakker.me>
2026-04-18 10:30:04 +0200

    Remove unfinished TUI

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # gotchet [![build](https://github.com/alexbakker/gotchet/actions/workflows/build.yml/badge.svg)](https://github.com/alexbakker/gotchet/actions/workflows/build.yml) [![docker image](https://img.shields.io/docker/v/alexanderbakker/gotchet?logo=docker&label=image)](https://hub.docker.com/r/alexanderbakker/gotchet/tags)
 
-__gotchet__ is a test report tool for Go. It can display test results in a TUI
-and generate HTML reports. Its primary purpose is to make it a bit easier to
-analyze results of large long-running test suites that make use of subtests and
-have lots of log output.
+__gotchet__ is a test report tool for Go. It can generate HTML reports for test
+results. Its primary purpose is to make it a bit easier to analyze results of
+large long-running test suites that make use of subtests and have lots of log
+output.
 
 ## Usage
 
--- cmd/gotchet/cmd/root.go
+++ cmd/gotchet/cmd/root.go
@@ -1,11 +1,6 @@
 package cmd
 
 import (
-	"fmt"
-	"os"
-
-	"github.com/alexbakker/gotchet/internal/tui"
-	tea "github.com/charmbracelet/bubbletea"
 	"github.com/spf13/cobra"
 )
 
@@ -13,30 +8,5 @@ var (
 	Root = &cobra.Command{
 		Use:   "gotchet",
 		Short: "Tool for reporting on Go test results",
-		Run:   startRoot,
 	}
-	rootFlags = struct {
-		Input   string
-		Emulate bool
-	}{}
 )
-
-func init() {
-	Root.Flags().StringVarP(&rootFlags.Input, "input", "i", "-", "input filename (or - for stdin)")
-	Root.Flags().BoolVarP(&rootFlags.Emulate, "emulate", "e", false, "emulate run time of the test report (useful for development)")
-}
-
-func startRoot(cmd *cobra.Command, args []string) {
-	capture := runCapture(rootFlags.Input, rootFlags.Emulate)
-
-	p := tea.NewProgram(tui.New(capture),
-		tea.WithMouseCellMotion(),
-		tea.WithAltScreen(),
-	)
-	if _, err := p.Run(); err != nil {
-		fmt.Fprintf(os.Stderr, "error: %v", err)
-		os.Exit(1)
-	}
-
-	fmt.Fprintf(os.Stderr, "bye!")
-}
